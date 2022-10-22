07da57017ce2f81a8c09b47f8fd10bc505f32c1f
CygnusX-26 <qh4nveh@gmail.com>
2022-10-21 21:01:47 -0400

    a

--- bot/cogs/Bot.py
+++ bot/cogs/Bot.py
@@ -15,7 +15,18 @@ class Bot(commands.Cog):
             status=discord.Status.online,
             activity=discord.Game(f"Put your funny message here!"))
         print('This bot is online!')
+    
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author.name == "CygnusX":
+            await message.channel.send("Your message here")
+        pass
 
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.Member) -> None:
+        if reaction.emoji == "👍":
+            await reaction.message.channel.send(f"hello {user.name}")
+        pass
 
 
 async def setup(bot: commands.Bot):
